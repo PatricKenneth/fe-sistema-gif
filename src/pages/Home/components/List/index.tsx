be42ca49867325463daec86d957828fbd688e51e
Patric Kenneth <patrickenneth77@gmail.com>
2021-12-29 14:08:44 -0300

    configuring environment

---
+++ src/pages/Home/components/List/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+function List(): React.ReactElement {
+    return (
+        <p>List</p>
+    )
+}
+
+export default List;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import List from "./components/List";
+
+function Home(): React.ReactElement {
+    return (
+        <List />
+    )
+}
+
+export default Home;
